436a5c1cb8d9e744591e8c7e7879a6f3e7a5e7c9
798010 <798010@Ctsaus-LT-430.cts.com>
2019-08-01 11:12:57 +1000

    Added a comment into the Name.java file.

--- src/main/java/academy/ausgrads/example/entity/Name.java
+++ src/main/java/academy/ausgrads/example/entity/Name.java
@@ -5,29 +5,31 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+// Added a comment to this java file.
+
 @Entity
 @Table(name = "NAME")
 public class Name {
-  @Id
-  @Column(name = "NAME_ID")
-  private Long id;
+	@Id
+	@Column(name = "NAME_ID")
+	private Long id;
 
-  @Column(name = "NAME")
-  private String name;
+	@Column(name = "NAME")
+	private String name;
 
-  public Long getId() {
-    return id;
-  }
+	public Long getId() {
+		return id;
+	}
 
-  public void setId(Long id) {
-    this.id = id;
-  }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-  public String getName() {
-    return name;
-  }
+	public String getName() {
+		return name;
+	}
 
-  public void setName(String name) {
-    this.name = name;
-  }
+	public void setName(String name) {
+		this.name = name;
+	}
 }
